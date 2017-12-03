00990fa9b2bbb8404f5a7703dd6788ad0a50777d
marcelo-vp <marcelos.mail@gmail.com>
2017-12-03 16:47:23 -0200

    Question constructor and prototypes

---
+++ question.js
@@ -0,0 +1,11 @@
+// Building the question constructor
+function Question (text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+// Building the 'check answer' prototype method
+Question.prototype.isCorrectAnswer = function (choice) {
+    return this.answer === choice;
+}
